51c5b1388beacefe14a11958e18b54d179718dd4
allswellthatsmaxwell <allswellthatsmaxwell9@gmail.com>
2018-04-08 01:38:27 -0500

    notes work, db to app!!

--- app/src/main/java/com/baby/baby/PrintActivity.java
+++ app/src/main/java/com/baby/baby/PrintActivity.java
@@ -22,60 +22,28 @@ public class PrintActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_print);
         final TextView textView = (TextView) findViewById(R.id.textView);
-        String s = "hi";
-        textView.setText(s);
 
-        final FirebaseDatabase database = FirebaseDatabase.getInstance(); // getInstance("https://chrmid-52856.firebaseio.c");
-        DatabaseReference ref = database.getReference(); // no args means reference to top level
-        //Quequery = ref.endAt("Adam"); // bad?
-        //query.addListenerForSingleValueEvent(new ValueEventListener() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference();
 
-        ref.child("0").addValueEventListener(new ValueEventListener() {
+        ref.child("0").child("notes").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-
-                Iterable<DataSnapshot> notes = dataSnapshot.child("notes").getChildren();
+                Iterable<DataSnapshot> notes = dataSnapshot.getChildren();
                 final List<Note> noteObjects = new ArrayList<Note>();
                 for (DataSnapshot note : notes) {
-                    //Note noteObj = note.getValue(Note.class);
-                    String date    = (String) note.child("date").getValue();
+                    String date = (String) note.child("date").getValue();
                     String time = (String) note.child("time").getValue();
                     String text = (String) note.child("text").getValue();
                     String patient = (String) note.child("patient").getValue();
                     String caretaker = (String) note.child("caretaker").getValue();
-
-                    //boolean ex = note.child(note.getKey()).exists();
-                    //boolean ex = note.child("caretaker").exists();
-                    //textView.setText("current key: " + note.getKey() + " note exists: " + ex + " note contents: " + text);
                     Note noteObj = new Note(date, time, text, patient, caretaker);
                     noteObjects.add(noteObj);
-                    //textView.setText("note contents: " + noteObj.getCaretaker());
                 }
-                /*
-                Iterable<DataSnapshot> patients = dataSnapshot.child("patients").getChildren();
-                List<Patient> patientObjects = new ArrayList<Patient>();
-                for (DataSnapshot patient : patients) {
-                    String patientName = patient.getKey();
-
-                    patientObjects.add(new Patient(patientName, ));
-                }
-                */
-                int i = 0;
-                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
-                for(DataSnapshot singleSnapshot : children) {
-                    i += 1;
-                    String name = (String) singleSnapshot.getKey();
-                    System.out.println(name);
-                    //textView.setText("name is " + name + ", ok? Iteration " + i);
-                    //String message = (String) singleSnapshot.child("patients").getValue();
-                }
-
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) { }
         });
-
-        // textView.setText(s);
     }
 }
