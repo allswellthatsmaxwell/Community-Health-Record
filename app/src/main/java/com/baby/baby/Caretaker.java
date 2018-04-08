447e26876b5cb9954affdf75980ac113a39550e2
allswellthatsmaxwell <allswellthatsmaxwell9@gmail.com>
2018-04-08 02:27:58 -0500

    got caretakers from db and into object list

--- app/src/main/java/com/baby/baby/Caretaker.java
+++ app/src/main/java/com/baby/baby/Caretaker.java
@@ -1,8 +1,26 @@
 package com.baby.baby;
 
-public class Caretaker {
-    int access_level;
-    String name;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
-    public Caretaker() {}
+import java.util.List;
+
+public class Caretaker {
+    String name;
+    String role;
+    List<String> patients;
+    String access_level;
+    public Caretaker(String name, String role, List<String> patients, String access_level) {
+        this.name = name;
+        this.role = role;
+        this.patients = patients;
+        this.access_level = access_level;
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String toString() {
+        return name + " is a " + role + " who takes care of " + String.join(", ", patients);
+    }
 }
--- app/src/main/java/com/baby/baby/PrintActivity.java
+++ app/src/main/java/com/baby/baby/PrintActivity.java
@@ -1,5 +1,8 @@
 package com.baby.baby;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +16,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class PrintActivity extends AppCompatActivity {
@@ -45,5 +49,31 @@ public class PrintActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) { }
         });
+
+        ref.child("0").child("caretakers").addValueEventListener(new ValueEventListener() {
+            @TargetApi(Build.VERSION_CODES.O)
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> caretakers = dataSnapshot.getChildren();
+                final List<Caretaker> caretakerObjects = new ArrayList<Caretaker>();
+                for (DataSnapshot caretaker : caretakers) {
+                    String name = (String) caretaker.child("name").getValue();
+                    String role = (String) caretaker.child("role").getValue();
+                    String accessLevel = (String) caretaker.child("access_level").getValue();
+                    HashMap<String, String> patientsMap = (HashMap) caretaker.child("patients").getValue();
+                    List<String> patients = new ArrayList<String>(patientsMap.keySet());
+                    Caretaker caretakerObj = new Caretaker(name, role, patients, accessLevel);
+                    caretakerObjects.add(caretakerObj);
+                }
+                for (Caretaker caretakerObj : caretakerObjects) {
+                    Log.d("READ_SUCCESS", caretakerObj.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
     }
 }
